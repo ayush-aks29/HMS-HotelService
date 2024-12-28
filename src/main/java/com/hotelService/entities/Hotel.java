e2ba3f17bebb3fe2471dfb2c725d9bbbd646b356
Ayush Kumar Srivastava <ayushksrivastava29@gmail.com>
2024-12-28 23:31:31 +0530

    switched to application.properties instead if yaml file

--- src/main/java/com/hotelService/entities/Hotel.java
+++ src/main/java/com/hotelService/entities/Hotel.java
@@ -3,6 +3,7 @@ package com.hotelService.entities;
 import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 @Getter
@@ -13,6 +14,7 @@ import javax.persistence.Entity;
 @Builder
 public class Hotel {
 
+    @Id
     private String id;
     private String name;
     private String location;
